f2720a769a3b94e36942208f7bd9d1bda710c4cd
asraralbeshri <dr.asrar24@gmail.com>
2020-01-26 17:08:54 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardOThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardOThree);

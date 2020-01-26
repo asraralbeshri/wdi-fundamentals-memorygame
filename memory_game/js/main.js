671c7de79e0c611dd3d6042cc6905a9d77d0df71
asraralbeshri <dr.asrar24@gmail.com>
2020-01-26 17:51:43 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardOThree = "king";
-let cardFour = "king";
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardOThree);
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped "+cardOne);
+console.log("User flipped "+cardTwo);
+
+if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else {
+        alert("Sorry, try again.");
+
+    }
+}
